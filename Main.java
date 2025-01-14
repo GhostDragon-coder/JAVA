e7bd2f9f29b3fff9fae2373fcba944c3b7986f84
GhostDragon-coder <77894954+GhostDragon-coder@users.noreply.github.com>
2025-01-14 12:17:21 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,14 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        double num_double;
+        num_double = -4.9e307;
+
+        byte num_byte;
+        num_byte = 95;
+
+        System.out.println(num_double);
+        System.out.println(num_byte);
+    }
+}
